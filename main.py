5543b893adb0e982389d8a519833cb493d62b842
KielerSeaGull <joel@blaich.rocks>
2025-11-30 14:06:57 +0100

    Ch3 - L1 Print Report is working

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def get_book_text(path: str) -> str:
     return file_content
 
 def main():
+    path: str = "books/frankenstein.txt"
+    #wandelt text in ein dic char -> count um, um dieses dann in eine Liste aus 2er-Dict (nur Buchstaben) Char: char, Num: num umzuwandeln
+    li_letters = tran_dict_to_list_of_dict(get_symbols_count(get_book_text(path)))
+    print_report(get_book_text(path), li_letters)
+
     #num_words: int = get_num_words(get_book_text("books/frankenstein.txt"))
     #num_symbols: dict[str, int] = get_symbols_count(get_book_text("books/frankenstein.txt"))
     #print(f"Found {num_words} total words")
--- stats.py
+++ stats.py
@@ -12,21 +12,25 @@ def get_symbols_count(text:str) -> dict[str, int]:
             wordcount[letter] = 1
         else:
             wordcount[letter] += 1
+   # print(f"{wordcount=}")
     return wordcount
 
 def sort_on(item: dict[str, int]) -> int:
     return item["num"]
 
 def tran_dict_to_list_of_dict(symbol_count: dict[str, int]) -> list[dict[str, Any]]:
+   # print(f"{symbol_count=}")
     list_of_dicts: list[dict[str, Any]] = []
     for char, num in symbol_count.items():
         if char.isalpha():
             item = {"char": char, "num": num}
-            list_of_dicts.append(item)   
-    return list_of_dicts.sort(reverse = True, key = sort_on)
+            list_of_dicts.append(item) 
+    list_of_dicts.sort(reverse = True, key = sort_on)  
+    return list_of_dicts
 
 def print_report(booktext: str, list_of_dict: list[dict[str, Any]]) -> None:
     num_words: int = get_num_words(booktext)
+    #print(f"{list_of_dict=}")
     #num_symbols: dict[str, int] = get_symbols_count(get_book_text(path_to_book): str)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at frankenstein.txt...")
